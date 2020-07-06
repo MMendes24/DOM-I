2086ea0a74ec95dde009b3240a7f7ec89810613f
MMendes24 <mmbusiness1@protonmail.com>
2020-07-06 15:18:37 -0400

    Main body and contact done. Working on footer.

--- js/index.js
+++ js/index.js
@@ -42,10 +42,46 @@ let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
 const topNav = document.querySelectorAll('a');
-topNav[0].textContent = "Services"
-topNav[1].textContent = "Product"
-topNav[2].textContent = "Vision"
-topNav[3].textContent = "Features"
-topNav[4].textContent = "About"
-topNav[5].textContent = "Contact"
+topNav[0].textContent = siteContent.nav["nav-item-1"]
+topNav[1].textContent = siteContent.nav["nav-item-2"]
+topNav[2].textContent = siteContent.nav["nav-item-3"]
+topNav[3].textContent = siteContent.nav["nav-item-4"]
+topNav[4].textContent = siteContent.nav["nav-item-5"]
+topNav[5].textContent = siteContent.nav["nav-item-6"]
+
+const headerSection = document.querySelector('cta')
+
+const titleHeader = document.querySelector('h1')
+titleHeader.textContent = siteContent.cta.h1
+
+const headerImg = document.querySelector('#cta-img')
+headerImg.setAttribute('src', siteContent.cta["img-src"])
+
+const headerButton = document.querySelector('button')
+headerButton.textContent = siteContent.cta.button
+
+const mainContent = document.querySelectorAll('.text-content h4');
+mainContent[0].textContent = siteContent["main-content"]["features-h4"]
+mainContent[1].textContent = siteContent["main-content"]["about-h4"]
+mainContent[2].textContent = siteContent["main-content"]["services-h4"]
+mainContent[3].textContent = siteContent["main-content"]["product-h4"]
+mainContent[4].textContent = siteContent["main-content"]["vision-h4"]
+
+const mainText = document.querySelectorAll('.text-content p');
+mainText[0].textContent = siteContent["main-content"]["features-content"]
+mainText[1].textContent = siteContent["main-content"]["about-content"]
+mainText[2].textContent = siteContent["main-content"]["services-content"]
+mainText[3].textContent = siteContent["main-content"]["product-content"]
+mainText[4].textContent = siteContent["main-content"]["vision-content"]
+
+const contentImg = document.querySelector('.middle-img')
+contentImg.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+
+const contactHeader = document.querySelector('.contact h4');
+contactHeader.textContent = siteContent.contact["contact-h4"]
+
+const contactParas = document.querySelectorAll('.contact p');
+contactParas[0].textContent = siteContent.contact.address
+contactParas[1].textContent = siteContent.contact.phone
+contactParas[2].textContent = siteContent.contact.email;
 
